9c0f47c15191903634890ece1c0691ee6e78e335
nrenner <nrenner0211@gmail.com>
2022-09-17 21:14:16 -0400

    functional save before implementing reactstrap

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import vault from "../../assets/content/vault.gif";
+import dadApp from "../../assets/content/dad-app.gif";
 
 function Gallery() {
   return (
-    <section>
-      <h1>NAME</h1>
-      <p>DESCRIPTION</p>
+
+    <section className="hero">
+
+      <div className="flex-row">
+
+          {/* vault */}
+      <h1 className="title">Vault</h1>
+      <p className="subtitle">A secure password manager</p>
+          <img
+            src={vault}
+            alt="Vault application"
+            className="img-thumbnail mx-1"
+          />
+
+          {/* dad-app */}
+
+          <img
+            src={dadApp}
+            alt="dad-app"
+            className="img-thumbnail mx-1"
+          />
+
+      </div>
     </section>
+
   );
 }
 export default Gallery;
