9e0c53a12fe14dadbc2aed74e315080a7636e626
nrenner <nrenner0211@gmail.com>
2022-09-18 16:51:04 -0400

    anotha one

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { Card, CardBody, CardTitle, CardSubtitle, CardText, CardLink, } from 'reactstrap';
 import vault from "../../assets/content/vault.gif"
 import dadApp from "../../assets/content/dad-app.gif";
+import portfolio from "../../assets/content/portfolio-1.gif"
 
 function Gallery() {
 
@@ -12,11 +13,18 @@ function Gallery() {
       <section className='flex-row'>
         <div className='card'>
           <div className='cardTitle'>Vault</div>
-            <a href="https://lh3.googleusercontent.com/dfd3cFIGjuL4RFU62v9l-K51HaYtM-3J8DnPpMcX6L0nnk-mQST6cCfwBm0PEWtCPFY=w2400?source=screenshot.guru"> <img alt="Vault Demo" src={ vault } /> </a>
+            <img alt="Vault Demo" src={ vault } />
+            <a href="https://safe-escarpment-80538.herokuapp.com/">Visit Vault</a>
         </div>
         <div className='card'>
           <div className='cardTitle'>dad-app</div>
-            <a href="https://nrenner0211.github.io/dad_app/"> <img alt="dad-app Demo" src ={dadApp} /> </a>
+            <img alt='dad-app Demo' src ={dadApp} />
+            <a href='https://nrenner0211.github.io/dad_app/'>Visit dad-app</a>
+        </div>
+        <div className='card'>
+          <div className='cardTitle'>Portfolio</div>
+            <img alt='portfolio demo' src={ portfolio } />
+            <a href='https://nrenner0211.github.io/my-first-portfolio/'>Visit Portfolio</a>
         </div>
       </section>
     </section>
