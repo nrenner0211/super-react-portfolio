b29d7314c11dc09a6ac084201d74bb9d235fa0e1
nrenner <nrenner0211@gmail.com>
2022-09-20 14:28:05 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../public/serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
