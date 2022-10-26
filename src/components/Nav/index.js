57ef15a97d50eebf9815b608c818e25972603435
nrenner <nrenner0211@gmail.com>
2022-10-26 19:52:02 -0400

    fix

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import favicon from '../../assets/cover/favicon.png';
+import favicon from '../../assets/cover/favicon.ico';
 import { FaHome } from "react-icons/fa";
 
 function Nav() {
