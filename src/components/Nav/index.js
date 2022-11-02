cc65b0305da60095971a2e09aaa4ca085296d8d1
nrenner <nrenner0211@gmail.com>
2022-11-02 12:26:53 -0400

    deploy

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { useState } from 'react';
 import { Link } from 'react-router-dom';
+// import { useState } from 'react';
 // import { FaHome } from "react-icons/fa";
 
 function Nav() {
